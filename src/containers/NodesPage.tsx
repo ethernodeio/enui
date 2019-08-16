b51d3685c6d987901b32164f62c0ba8cb263137c
bakon <maciej@ethernode.io>
2019-08-16 11:21:51 -0500

    fix: more fixes and test for router and GH pages

--- src/containers/NodesPage.tsx
+++ src/containers/NodesPage.tsx
@@ -76,7 +76,7 @@ const NodesPage: React.FC<IProps> = (props) => {
       const getUserInfo = await EnAPIhttp.getUser(token, username);
       setNodes(getUserInfo.nodes);
       setMounted(false);
-      // props.history.go("/nodes");
+      props.history.go("/nodes");
     } else {
       console.log(addNodeResult.message);
       setResult(addNodeResult.message);
