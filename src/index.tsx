7abf63b55af50b1485cd82663888e17b6e02f234
bakon <maciej@ethernode.io>
2019-08-16 09:56:38 -0500

    fix: testing other router options on gh-pages

--- src/index.tsx
+++ src/index.tsx
@@ -6,18 +6,18 @@ import LoginPage from "./containers/LoginPage";
 import RegisterPage from "./containers/RegisterPage";
 import MainPage from "./containers/MainPage";
 import NodesPage from "./containers/NodesPage";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 const routing = (
   <ReusableProvider>
-    <Router basename={"/"}>
+    <BrowserRouter basename={"/"}>
       <Switch>
         <Route path="/" exact component={MainPage} />
         <Route path="/login" component={LoginPage} />
         <Route path="/register" component={RegisterPage} />
         <Route path="/nodes" component={NodesPage} />
       </Switch>
-    </Router>
+    </BrowserRouter>
   </ReusableProvider>
 );
 
