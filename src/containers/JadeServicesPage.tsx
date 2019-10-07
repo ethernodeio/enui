fe25931da8289b40e6c37cf802f3dda584bc0b46
bakon <maciej@ethernode.io>
2019-10-07 18:08:49 -0500

    fix: no release yet lots of addons for service-runner-support

--- src/containers/JadeServicesPage.tsx
+++ src/containers/JadeServicesPage.tsx
@@ -9,7 +9,13 @@ import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
 import AddBoxIcon from "@material-ui/icons/AddBox";
-import DirectionsRunIcon from '@material-ui/icons/DirectionsRun';
+import DirectionsRunIcon from "@material-ui/icons/DirectionsRun";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import Avatar from "@material-ui/core/Avatar";
+import StorageIcon from "@material-ui/icons/Storage";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -59,34 +65,69 @@ const JadeServicesPage: React.FC = () => {
 
   useEffect(() => {
     listServices();
-  }, [services.length]);
+  }, [services.length, runningServices.length, installedServices.length]);
 
   return (
     <div className={classes.root}>
-      <NavigationBar title={"Home - Welcome to enUI " + username} />
+      <NavigationBar title={"Jade Services - for user: " + username} />
       <Grid container spacing={1}>
         <Grid item xs={3}>
           <Paper className={classes.paper}>
             <div>
               <Typography variant="h5">Services Available: {services.length}</Typography>
             </div>
-            {
-              services && services.map((service: { name: any }, index: any) => (
-                <div id={index}>Name: {service.name}</div>
-              ))
-            }
+            <List className={classes.root}>
+              {
+                services && services.map((service: { name: any, version: any }, index: any) => (
+                  <ListItem>
+                    <ListItemAvatar>
+                      <Avatar>
+                        <StorageIcon />
+                      </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText primary={service.name} secondary={service.version} />
+                  </ListItem>
+                ))
+              }
+            </List>
           </Paper>
         </Grid>
         <Grid item xs={3}>
           <Paper className={classes.paper}>
-            <Typography variant="h5">Services Running</Typography>
-            <Typography>{runningServices.length}</Typography>
+            <Typography variant="h5">Services Running: {runningServices.length}</Typography>
+            <List className={classes.root}>
+              {
+                runningServices && runningServices.map((serviceRunning: { name: any, version: any, state: any }, index: any) => (
+                  <ListItem>
+                    <ListItemAvatar>
+                      <Avatar>
+                        <StorageIcon />
+                      </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText primary={serviceRunning.name} secondary={serviceRunning.version} />
+                  </ListItem>
+                ))
+              }
+            </List>
           </Paper>
         </Grid>
         <Grid item xs={3}>
           <Paper className={classes.paper}>
-            <Typography variant="h5">Services Installed</Typography>
-            <Typography>{installedServices.length}</Typography>
+            <Typography variant="h5">Services Installed: {installedServices.length}</Typography>
+            <List className={classes.root}>
+              {
+                installedServices && installedServices.map((installedService: { name: any, version: any, state: any }, index: any) => (
+                  <ListItem>
+                    <ListItemAvatar>
+                      <Avatar>
+                        <StorageIcon />
+                      </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText primary={installedService.name} secondary={installedService.version} />
+                  </ListItem>
+                ))
+              }
+            </List>
           </Paper>
         </Grid>
       </Grid>
